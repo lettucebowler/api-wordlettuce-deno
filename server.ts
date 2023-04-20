3234108f1d0a5d5de80feae0a20d56cdd38a9b38
Grant Montgomery <lettucebowler@gmail.com>
2023-04-19 22:14:59 -0500

    bleh

---
+++ server.ts
@@ -0,0 +1,8 @@
+import { serve } from 'https://deno.land/std@0.167.0/http/server.ts';
+import { Hono } from 'npm:hono@3.1.0';
+
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello Hono!'));
+
+serve(app.fetch);
