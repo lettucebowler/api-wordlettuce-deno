4523d4b87f984d4c253ea3f36b089ba7be2591bd
Grant Montgomery <lettucebowler@gmail.com>
2023-04-29 20:53:40 -0500

    remove example server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'npm:hono';
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { zValidator } from './middleware/validator.ts';
+import { zValidator } from './middleware/validator.js';
 import { bearerAuth } from 'npm:hono/bearer-auth';
 import "https://deno.land/x/dotenv/load.ts";
 
@@ -15,7 +15,7 @@ app.use('/*', async (c, next) => {
 // import { getRankingsRequestSchema, getRankings } from './controller/rankings.ts';
 // app.get('/v1/rankings', zValidator('query', getRankingsRequestSchema), getRankings);
 
-import { putUser, putUserParamSchema, putUserBodySchema } from './controller/users.ts';
+import { putUser, putUserParamSchema, putUserBodySchema } from './controller/users.js';
 app.put(
 	'/v1/users/:id',
 	zValidator('param', putUserParamSchema),
@@ -25,7 +25,7 @@ app.put(
 // import { userFilterSchema, getUsers } from './controller/users.ts';
 // app.get('/v1/users', zValidator('query', userFilterSchema), getUsers);
 
-import { getUserRequestSchema, getUser } from './controller/users.ts';
+import { getUserRequestSchema, getUser } from './controller/users.js';
 app.get('/v1/users/:id', zValidator('param', getUserRequestSchema), getUser);
 
 // import { getUserGameResults } from './controller/users.ts';
